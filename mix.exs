d122db14d131d9884ec49f85f8c87128a7cb8285
shadowlegend <lyheng0@gmail.com>
2025-10-10 17:27:04 +0700

    chore: update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OryKratosAdmin.MixProject do
   def project do
     [
       app: :ory_kratos_admin,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
